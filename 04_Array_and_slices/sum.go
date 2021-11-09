47f1a7047d1370468c261bf6361c2f67cd4fe8d2
manudevelopia <manu@developia.info>
2021-11-09 15:08:03 +0100

    replicate code using slices

--- 04_Array_and_slices/sum.go
+++ 04_Array_and_slices/sum.go
@@ -1,6 +1,14 @@
 package arrays
 
-func Sum(numbers [5]int) int {
+func SumFixedSize(numbers [5]int) int {
+	result := 0
+	for _, number := range numbers {
+		result += number
+	}
+	return result
+}
+
+func Sum(numbers []int) int {
 	result := 0
 	for _, number := range numbers {
 		result += number
