ee9770488497ed9f55eb863db44f859c92fbc937
manudevelopia <manu@developia.info>
2021-11-12 22:44:42 +0100

    add perimeter and area functions

---
+++ 05_structs/perimeter.go
@@ -0,0 +1,9 @@
+package structs
+
+func Perimeter(width float64, height float64) float64 {
+	return 2 * (width + height)
+}
+
+func Area(width float64, heigth float64) interface{} {
+	return width * heigth
+}
